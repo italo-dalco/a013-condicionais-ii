3ce6160ad8f95d78a1fadf7a5a6351a954f21c87
italo-dalco <italo.bsd@gmail.com>
2022-09-08 16:31:00 -0300

    italo-dalco-exercicio-aula13

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,3 +1,11 @@
+//# Exercício 2
+
+// Recrie o exercício 3 da aula anterior (A012) de condicionais I,
+// utilizando switch-case. No arquivo `script,js` existe uma versão
+// do código já escrita, mas você pode colar o exercício que você
+// fez na última prática!
+
+
 let nacionalidade = prompt("digite aqui a nacionalidade").toLowerCase();
 
 if(nacionalidade === "brasileira"){
@@ -13,3 +21,28 @@ if(nacionalidade === "brasileira"){
 } else{
     console.log("nacionalidade não encontrada")
 }
+
+
+// -----------------------------------------------------------------
+
+
+switch(nacionalidade){
+    case "brasileira":
+        console.log("a pessoa é do Brasil!");
+        break
+    case "argentina" :
+        console.log("a pessoa é da Argentina!");
+        break
+     case "uruguaia" :
+        console.log("a pessoa é do Uruguai!");
+        break
+     case "chilena" :
+        console.log("a pessoa é do Chile!");
+        break
+     case "colombiana" :
+        console.log("a pessoa é da Colômbia!");
+        break    
+     default :
+     console.log("nacionalidade não encontrada")
+    
+}   
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,4 +1,23 @@
+// # Exercício 3
+
+// Reescreva os códigos do arquivo script.js utilizando o if ternário.
+
+// **Relembrando o if ternário**
+// ```jsx
+// condição ? expr1 : expr2
+// ```
+
+// Onde :
+
+// `condição` é uma expressão que é avaliada como `true` ou `false`.
+
+// `expr1`, `expr2` são xpressões com valores de qualquer tipo.
+
+
+
 //código a ser reescrito
+
+const nome = prompt("Insira seu nome")
 
 if(nome === "José"){
 	console.log("Oi, Zé!");
@@ -6,8 +25,25 @@ if(nome === "José"){
 	console.log("Olá, "+nome);
 }
 
+let idade = prompt("Insira sua idade")
+
 if(idade >= 18){
 	console.log("pode tirar carteira de motorista!");
 } else {
 	console.log("Ainda não pode tirar carteira de motorista");
 }
+
+
+// Reescreva os códigos do arquivo script.js utilizando o if ternário.
+
+nome === "José" ? console.log("Oi, Zé!") : console.log("Olá, " + nome);
+
+idade >= 18 ? console.log("pode tirar carteira de motorista!") : console.log("Ainda não pode tirar carteira de motorista")
+
+
+
+
+// const idadeDependente = Number(prompt("Qual a idade do dependente?"))
+
+// idadeDependente === 13 ? console.log("A idade do dependente é 13 e já pode ter um cartão.")
+// : console.log("Verifique as opções possíveis.")
